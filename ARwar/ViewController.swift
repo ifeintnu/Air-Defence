a073edb811950873f425c617b65d72f1385b6f38
jackyhobingo <jackyhobingo@gmail.com>
2018-06-08 18:11:42 +0800

    Complete getCamera position method

--- ARwar/ViewController.swift
+++ ARwar/ViewController.swift
@@ -11,17 +11,33 @@ import ARKit
 
 let Debug = true
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
 
     
     @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var button: UIButton!
+
+    @IBAction func btnClick(_ sender: Any) {
+        print(getComeraPosition()?.x, getComeraPosition()?.y, getComeraPosition()?.z)
+    }
+    
+    func getComeraPosition()->SCNVector3? {
+        let currentTransform = self.sceneView.session.currentFrame?.camera.transform
+        guard let columns = currentTransform?.columns else {
+            return nil
+        }
+        return SCNVector3((columns.3.x), (columns.3.y), (columns.3.z))
+    }
+    
     let configuration = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
         if (Debug) {
             self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         }
+        self.sceneView.delegate = self
         self.sceneView.session.run(configuration)
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
